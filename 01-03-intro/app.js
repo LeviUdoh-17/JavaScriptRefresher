6561ecb1fc34f4cedffc0f77c565a8aafea306e3
LeviUdoh-17 <leviudoh17@gmail.com>
2025-01-14 21:46:26 +0100

    Add initial JavaScript and HTML files for basic exercises and variable manipulation

---
+++ 01-03-intro/app.js
@@ -0,0 +1,79 @@
+// Use alert(...) to display 'Good Morning' in a popup
+alert('Good Morning');
+
+// Display your name in a popup
+alert('Udoh Ukaraobong Levi');
+
+// Using math, calculate 10 + 5 
+console.log(10 + 5);
+
+// Using math, calculate 20 - 5
+console.log(20 - 5);
+
+// Using math, calculate 2 + 2 - 5
+console.log(2 + 2 - 5);
+
+// Use document.body.innerHTML to display 'Good Morning' on the page
+document.body.innerHTML = 'Good Morning';
+
+// Display your name on the webpage
+document.body.innerHTML = 'Udoh Ukaraobong Levi';
+
+// You order a T-Shirt for $10, socks for $8, and a dinning plate for $20. Calculate the total cost
+console.log('Total Cost: $10 + $8 + $20 = $' + (10 + 8 + 20));
+
+// Your bank account has $100, you spend $20 on lunch, $50 on dinner, and earn $200 from a job, calculate your new balance
+console.log('New Balance: $100 - $20 - $50 + $200 = $' + (100 - 20 - 50 + 200));
+
+// Use document.body.innerHTML to make your webpage blank
+document.body.innerHTML = '';
+
+// ====================================================================================================
+// MATH AND NUMBERS
+// ====================================================================================================
+
+// At a resturant, you order 1 soup for $10, and 1 ice cream for $5. Calculate the total cost
+console.log('Total Cost: $10 + $5 = $' + (10 + 5));
+
+// You're at a resturant with 2 friends(3 people total). We made the same order as the previous question. Calculate the total cost
+console.log('Total Cost: ($10 + $5)*3 = $' + (10 + 5)*3);
+
+// Calculate the total cost of a toaster ($18.50) and 2 shirts($7.50 each)
+console.log('Total Cost: $18.50 + ($7.50*2) = $' + (18.50 + (7.50*2)));
+
+// Calculate 10% of the previous total cost
+console.log('10% of (18.50 + (7.50*2)) = $' + (0.10 * (18.50 + (7.50*2))));
+
+// Calculate a 20% tax on the previous total cost
+console.log('20% of (18.50 + (7.50*2)) = $' + (0.20 * (18.50 + (7.50*2))));
+
+// The temperature is 25 degrees celsius. Convert it to fahrenheit
+console.log('25 degrees celsius in fahrenheit = ' + (25 * 9/5 + 32));
+
+// The temperature is 86 degrees fahrenheit. Convert it to celsius
+console.log('86 degrees fahrenheit in celsius = ' + ((86 - 32) * 5/9));
+
+//  The temperature is -5 degrees celsius. Convert it to fahrenheit
+console.log('-5 degrees celsius in fahrenheit = ' + (-5 * 9/5 + 32));
+
+// ====================================================================================================
+// STRINGS
+// ====================================================================================================
+
+// Create the text "My name is:" as a string
+console.log('My name is:');
+
+// Create your name as a string
+console.log('Udoh Ukaraobong Levi');
+
+// Using concatenation, display "My name is: Udoh Ukaraobong Levi"
+console.log('My name is: ' + 'Udoh Ukaraobong Levi');
+
+// At a restaurant, you order 1 coffee($5) and 1 bagel ($3). Using math, calculate the total cost using concatenation, create the text
+console.log('Total Cost: $' + (5 + 3));
+
+// Do the same calculation as before using template strings and interpolation to display the text
+console.log(`Total Cost: $${5 + 3}`);
+
+// Display the previous text as an alert
+alert(`Total Cost: $${5 + 3}`);
